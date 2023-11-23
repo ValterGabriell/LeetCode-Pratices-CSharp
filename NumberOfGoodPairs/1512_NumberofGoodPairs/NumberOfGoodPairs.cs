f0a84e35381c04c6b6f84ec8b8a408af2fba38d3
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-11-23 14:06:12 -0300

    Initial commit

---
+++ NumberOfGoodPairs/1512_NumberofGoodPairs/NumberOfGoodPairs.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode._1512_NumberofGoodPairs
+{
+    public class NumberOfGoodPairs
+    {
+        //os 2 numeros precisam ser iguals e o indice I precisa ser menor que o J
+        // [1, 2, 3, 1, 2, 1] - o par (0, 3)
+        // é bom porque os números nessas posições são iguais (ambos são 1) e 0 é menor que 3.
+        public int NumIdenticalPairs(int[] nums)
+        {
+            
+        }
+    }
+}
