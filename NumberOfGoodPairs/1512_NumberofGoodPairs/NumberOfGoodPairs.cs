7050e842aff14fd32a8cc5781ea9b97200cfae5d
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-11-23 20:20:55 -0300

    1512

--- NumberOfGoodPairs/1512_NumberofGoodPairs/NumberOfGoodPairs.cs
+++ NumberOfGoodPairs/1512_NumberofGoodPairs/NumberOfGoodPairs.cs
@@ -1,13 +1,30 @@
-﻿namespace LeetCode._1512_NumberofGoodPairs
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._1512_NumberofGoodPairs
 {
     public class NumberOfGoodPairs
     {
-        //os 2 numeros precisam ser iguals e o indice I precisa ser menor que o J
-        // [1, 2, 3, 1, 2, 1] - o par (0, 3)
-        // é bom porque os números nessas posições são iguais (ambos são 1) e 0 é menor que 3.
         public int NumIdenticalPairs(int[] nums)
         {
-            
+            var countByValue = new Dictionary<int, int>();
+            var goodPairs = 0;
+
+            foreach (var num in nums)
+            {
+                if (countByValue.ContainsKey(num))
+                {
+                    goodPairs += countByValue[num];
+                    countByValue[num]++;
+                }
+                else
+                {
+                    countByValue[num] = 1;
+                }
+            }
+
+            return goodPairs;
         }
     }
 }
--- NumberOfGoodPairs/Program.cs
+++ NumberOfGoodPairs/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace LeetCode
+﻿using System;
+using LeetCode._1512_NumberofGoodPairs;
+
+namespace LeetCode
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            var numberOfGoodPairs = NumberOfGoodPairs();
+            Console.WriteLine("Numeros de pares bons: " + numberOfGoodPairs);
+        }
+
+        public static int NumberOfGoodPairs()
+        {
+            var numberOfGoodPais = new NumberOfGoodPairs();
+            var array = new[] { 1,2,3};
+            var numIdenticalPairs = numberOfGoodPais.NumIdenticalPairs(array);
+            return numIdenticalPairs;
+        }
     }
-  }
 }
