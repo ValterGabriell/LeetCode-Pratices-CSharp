1260d4cf8d12f7916743bd8bc31969617eb5b20f
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-12-04 22:51:46 -0300

    1365

---
+++ NumberOfGoodPairs/1365-how-many-numbers-are-smaller-than-the-current-number/HMNASTCN.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace LeetCode._1365_how_many_numbers_are_smaller_than_the_current_number
+{
+    public class HMNASTCN
+    {
+        public int[] testNumbers(int[] nums)
+        {
+            var result = new int[nums.Length];
+            for (var currentIndexToBeTested = 0; currentIndexToBeTested < nums.Length; currentIndexToBeTested++)
+            {
+                var currentMainlyValue = nums[currentIndexToBeTested];
+                var repeatTimes = 0;
+                for (var indexToBeTestedWith = 0; indexToBeTestedWith < nums.Length; indexToBeTestedWith++)
+                {
+                    var currentTestValue = nums[indexToBeTestedWith];
+                    if (currentIndexToBeTested == indexToBeTestedWith) continue;
+                    if (currentTestValue < currentMainlyValue)
+                    {
+                        repeatTimes++;
+                    }
+
+                    result[currentIndexToBeTested] = repeatTimes;
+                }
+            }
+            return result;
+        }
+    }
+}
--- NumberOfGoodPairs/Program.cs
+++ NumberOfGoodPairs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using LeetCode._1365_how_many_numbers_are_smaller_than_the_current_number;
 using LeetCode._1512_NumberofGoodPairs;
 
 namespace LeetCode
@@ -7,16 +9,25 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            var numberOfGoodPairs = NumberOfGoodPairs();
-            Console.WriteLine("Numeros de pares bons: " + numberOfGoodPairs);
+            var hmnastcn = HMNASTCN();
+            string[] toString = Array.ConvertAll(hmnastcn, value => value.ToString());
+            string array = string.Join(",", toString);
+            Console.WriteLine(array);
         }
 
         public static int NumberOfGoodPairs()
         {
             var numberOfGoodPais = new NumberOfGoodPairs();
-            var array = new[] { 1,2,3};
+            var array = new[] { 1, 2, 3 };
             var numIdenticalPairs = numberOfGoodPais.NumIdenticalPairs(array);
             return numIdenticalPairs;
+        }
+
+        public static int[] HMNASTCN()
+        {
+            var hmn = new HMNASTCN();
+            var array = new[] { 8,1,2,2,3 };
+            return hmn.testNumbers(array);
         }
     }
 }
